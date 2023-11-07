b1b65b1946146e069559086a0b7559c056f1249a
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2023-11-07 10:43:52 +0100

    Do some CORS setup

--- frontend/api.js
+++ frontend/api.js
@@ -1,6 +1,13 @@
-function productTable() {
+async function productTable() {
     let wrapper = document.getElementById("products-table");
     let table = document.createElement('table')
+
+    let json = await fetch("http://localhost:8877/products")
+        .then(data => data.json)
+
+    for (let product in json) {
+        console.log(product)
+    }
 
     wrapper.appendChild(table)
 }
--- src/main/kotlin/dev/vanadium/wmc/backend/security/SecurityConfig.kt
+++ src/main/kotlin/dev/vanadium/wmc/backend/security/SecurityConfig.kt
@@ -4,17 +4,37 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
 
     @Bean
+    fun corsConfig(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("GET", "POST")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
     fun configSecurity(http: HttpSecurity): SecurityFilterChain {
-        return http
-            .cors { cors -> cors.disable() }
-            .build()
+        http {
+            securityMatcher("/**")
+            cors { disable() }
+            csrf { disable() }
+            authorizeHttpRequests {
+                authorize(anyRequest, permitAll)
+            }
+        }
+        return http.build()
     }
 
 }
